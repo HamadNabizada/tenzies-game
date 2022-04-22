f4159b7199d4d951451f4e420f086fa82c2b1387
Hamad <HamadNabizada@gmail.com>
2022-04-22 07:40:23 -0700

    sixDiceArray state is saved as an object of arrays. updated code to work with the changes

--- src/Tenzies.js
+++ src/Tenzies.js
@@ -1,30 +1,30 @@
 import React, {useState, useEffect} from 'react'
 import Dice from './Dice'
+import {nanoid} from "nanoid"
 
 export default function Tenzies(){
-
     function getRandomDice(numOfDice){
-        let tempDiceArray = []
-        let diceCounter=0
+        let diceArray=[]
         for (let i = 0; i < numOfDice; i++) {
-            tempDiceArray.push(Math.floor(Math.random() * 6)+1)
-            diceCounter++
+            let tempDie = {
+                value: (Math.floor(Math.random() * 6)+1),
+                isHeld: false,
+                id: nanoid()
+            }
+            diceArray.push(tempDie)
         }
-        diceCounter=0
-        return  tempDiceArray
+        return diceArray
     }
 
     let [sixDiceArray, setSixDiceArray] = useState(getRandomDice(10))
-
-    let diceElements = sixDiceArray.map((item,index) =>{
-        return <Dice key={index} value={item}/>
+    let diceElements = sixDiceArray.map((item) =>{
+        return <Dice key={item.id} value={item.value}/>
     })
-    
+
     function generateNewDice(){
-        setSixDiceArray(prevArray => {
-            return getRandomDice(10)
-        })
+        setSixDiceArray(getRandomDice(10))
     }
+    
 
     return (
         <section className="tenzies-wrapper">
